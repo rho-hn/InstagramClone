f241288de763db2ca2d6318f5941e521152ac100
Rohan Srivastava <notrohansrivastava@gmail.com>
2021-12-21 01:25:55 +0530

    i

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
 import "./App.css";
 import Post from "./Post";
 import React, { useState, useEffect } from "react";
-import { db } from "./firebase";
+import { db , auth } from "./firebase";
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button , Input } from '@material-ui/core';
+
+
+
+
 
 function App() {
-  const [posts, setPosts] = useState([
-    /*{
-      imageUrl:"https://source.unsplash.com/500x500/?instagram",
-      userName:"pradipta-nag",
-      caption:"Working Post"
-    },
-    {
-      imageUrl:"https://source.unsplash.com/500x500/?instagram",
-      userName:"keya-nag",
-      caption:"Working Post"
-    }*/
-  ]);
+ 
+  const [modalStyle]= useState(getModalStyle);
+  const [posts, setPosts] = useState([]);
+  const [open,setOpen]= useState(false);
+  const [username,setUsername]= useState('');
+  const [email,setEmail]= useState('');
+  const [password,setPassword]= useState('');
+ const [user,setUser]= useState(null);
+
+
+useEffect(()=>{
+auth.onAuthStateChanged((authUser)=>{
+if (authUser){
+  //user has logged in 
+ setUser(authUser);
+} else{
+  //user has logged out
+  setUser(null);
+}
+})
+},[user,username]);
+
+
+const useStyles = makeStyles((theme)=> ({
+  paper: {
+  position: 'absolute',
+  width: 400,
+  backgroundColor: theme.palette.background.paper,
+  bprder: theme.shadows[5],
+  padding: theme.spacing(2,4,3),
+}
+}))
+
+const classes = useStyles();
+
+  function getModalStyle(){
+  const top = '50%';
+  const left = '50%';
+
+  return {
+    top: '$(top)%',
+    left: '$(left)%',
+    transform: 'translate(-${top}%,-${left}%)',
+  }
+  }
 
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
@@ -28,14 +68,66 @@ function App() {
     });
   }, []);
 
+
+  const signUp= (event)=>{
+    event.preventDefault();
+    auth.createUserWithEmailAndPassword(email,password)
+    .then((authUser)=> {
+      return authUser.user.updateProfile({
+        displayName: username
+      })
+    })
+    .catch((err)=> alert(err.message))
+  }
+  
+  
   return (
     <div className="app">
-      <div className="app__header">
-        <img
+    <Modal
+    open={open}
+    onClose={()=> setOpen(false)}
+    >
+    <div style={modalStyle} className = {classes.paper}>
+    <center>
+    <h5> instagram </h5>
+    <form className= 'app___signup'>
+    <Input
+    type = "text"
+    placeholder = "username"
+    value = {username}
+    onChange = {(e)=>setUsername(e.target.value)}
+    />
+     <Input
+    type = 'text'
+    placeholder = 'email'
+    value = {email}
+    onChange = {(e)=>setEmail(e.target.value)}
+    />
+     <Input
+    type = 'text'
+    placeholder = 'password'
+    value = {password}
+    onChange = {(e)=>setPassword(e.target.value)}
+    />
+    <Button  type='submit' onClick= {signUp}> Sign Up </Button>
+    </form>
+    </center>
+    </div>
+    </Modal>
+
+          <div className="app__header">
+                <h3>instagram</h3>
+        {/* <img
           className="app__headerImage"
-          src="https://source.unsplash.com/100x50/?instagram"
+          src="https://source.instagram.com/static/?instagram"
           alt=""
-        />
+        /> */}
+        { user ? (
+          <Button onClick = {()=> auth.signOut()}>Log Out</Button> 
+        ):(
+        <Button onClick= {()=> setOpen(true)}>Sign Up </Button>
+        )}
+        
       </div>
       {posts.map(({id, post}) => (
         <Post
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCWQ3IxsjXPTxAHnFKp6TbvH9nNwjL39Rw",
-    authDomain: "instagram-clone-cd00f.firebaseapp.com",
-    projectId: "instagram-clone-cd00f",
-    storageBucket: "instagram-clone-cd00f.appspot.com",
-    messagingSenderId: "797474157428",
-    appId: "1:797474157428:web:eca6a9bca76f817890b221"
+  apiKey: "AIzaSyAVLsURPtL3lvy5FYy8he-FCr6cxWvnXOA",
+      authDomain: "clientreact-12045.firebaseapp.com",
+      projectId: "clientreact-12045",
+      storageBucket: "clientreact-12045.appspot.com",
+      messagingSenderId: "815509263499",
+      appId: "1:815509263499:web:5fad86509cc81a3e05708e",
+      measurementId: "${config.measurementId}"
   });
 
   const db = firebaseApp.firestore();
   const auth = firebaseApp.auth();
   const storage = firebaseApp.storage();
-
+// db.collection('posts').add({userName:"rohan"});
   export { db, auth, storage };
+
 
